e097c707cd103e34a7b7127b41aef2041f07e92e
Gabriel Lima Gomes <81588558+ghablima@users.noreply.github.com>
2023-05-26 18:20:50 -0300

    Add files via upload

---
+++ dados-ofertas.php
@@ -0,0 +1,31 @@
+<?php
+
+$ofertas = [   
+    [        
+        "nome" => "Capitães da Areia - Jorge Amado",
+        "preco" => "R$ 30,00",
+        "foto" => "imagens/capitaes.jpg",
+        "descricao" => "Conheça o liquidificador da Mondial L-99 WB Turbo Power. Além de moderno e elegante na linda cor preta, ele conta com um novo sistema de encaixe pra deixar a montagem mais prática e fácil. Olha, o copo está super-resistente e transparente pra deixar os ingredientes mais visíveis. Sua capacidade total é de até 2,2 litros. ", 
+    ],
+
+    [
+        "nome" => "Galaxy S20 - Smartphone",
+        "preco" => "R$ 2.100,00",
+        "foto" => "imagens/s20.jpg",        
+        "descricao" => "Fogão 4 Bocas da Eletrolux. Com este super fogão você vai ter muito mais segurança e comodidade no dia a dia. Possui forno limpa fácil, puxador de aço, super-reforçado, além de botões removíveis facilitando a limpeza do produto e queimador super chama. Tem ainda acendimento total automático e pés altos.", 
+    ],
+    
+    [       
+        "nome" => "Assassins Creed - Ultimate Xbox One",
+        "preco" => "R$ 100,00",
+        "foto" => "imagens/jogo.jpg",        
+        "descricao" => "Para você que busca praticidade e melhor desempenho na hora de lavar suas roupas, a Lavadora de Roupas CWH12 Branca da Consul proporciona a melhor lavagem e o menor consumo de água no dia a dia. A lavadora possui capacidade de 12kg e 16 programas de lavagem, isso sem contar com a dosagem econômica que permite a economia de até 70% de sabão.",  
+    ],
+
+    [        
+        "nome" => "Cadeira Gamer - Modelo Racee",
+        "preco" => "R$ 1.000,00",
+        "foto" => "imagens/cadeira.jpg",
+        "descricao" => "Nos dias de hoje, ter eletrodomésticos que facilitam a rotina do dia a dia, trazem muitos benefícios. E com uma geladeira ou refrigerador não é diferente. Se você está a procura de uma nova ou quer trocar, precisa conhecer o refrigerador CRM44 da Consul.",
+    ]
+];
---
+++ login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+
+    <div class="superior">
+        <div class="alto">
+            <div class="icon">
+                <img src="imagens/face.png">
+            </div>
+            <h2>login do cliente</h2>
+        </div>
+
+        <div class="baixo">
+            <p>Bem vindo(a), preencha o formulário, informando o email e senha da sua conta, e tenha um atendimento personalizado</p>
+        </div>
+    </div>
+
+    <div class="form">
+        <div class="preencher">
+            <form action="pagina-de-resultado.php" method="POST">
+                <label>
+                    <div class="email">
+                        <p>e-mail*</p>
+                        <input type="search">
+                    </div>
+                    
+                    <div class="senha">
+                        <p>senha*</p>
+                        <input type="search">
+                    </div>
+                    
+                </label>
+            </form>
+        </div>
+        <a href="index.php">
+            <div class="botao">
+            <p>continuar</p>
+            </div>
+        </a>
+        
+    </div>
+
+	
+</body>
+</html>
