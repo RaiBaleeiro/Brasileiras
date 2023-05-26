dd12d084ba9d246334ea32dcf40231a373e8e6a7
Pedro Henrique <81756799+Pedrovesq@users.noreply.github.com>
2023-05-25 21:03:57 -0300

    Criação do arquivo dados.php

---
+++ Pagina-Principal/dados.php
@@ -0,0 +1,21 @@
+<?php
+
+$produtos = [
+    [
+        "name"=> "Kit 5 Camisetas Masculinas Básicas Manga Curta",
+        "price"=> 42.49,
+        "rating"=> 4,
+        "image"=> "https://raw.githubusercontent.com/Cassianosch/programador.cs-reels/develop/slider-produtos-simples/assets/images/1-1.webp"
+    ],
+    [
+        "name"=> "Nutella pote especial 350G ferrero",
+        "price"=> 19.99,
+        "rating"=> 5,
+        "image"=> "https://raw.githubusercontent.com/Cassianosch/programador.cs-reels/develop/slider-produtos-simples/assets/images/2-1.webp"
+    ],
+    [
+        "name"=> "Console Xbox Series S 500gb Ssd",
+        "price"=> 2276.10,
+        "rating"=> 5,
+        "image"=> "https://raw.githubusercontent.com/Cassianosch/programador.cs-reels/develop/slider-produtos-simp…"
+?>
